981b1a0733732986398ff87be8643606b9e15047
NandhipatiGanesh <nandhipatiganeshkumar16@gmail.com>
2023-08-08 15:59:13 +0530

    plans section added

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ function myFunction (x) {
         x.style.display = "block";
     }
 };
+/*tabs script*/
+function openTab(tabName) {
+    const tabs = document.querySelectorAll('.plans-li');
+    const tabContents = document.querySelectorAll('.monthly-plan-content, .yearly-plan-content');
+  
+    tabs.forEach(tab => tab.classList.remove('active'));
+    tabContents.forEach(content => content.classList.remove('active'));
+  
+    const selectedTab = document.getElementById(`${tabName}Tab`);
+    const selectedTabButton = document.getElementById(`tab${tabName === 'monthly' ? '1' : '2'}`);
+  
+    selectedTabButton.classList.add('active');
+    selectedTab.classList.add('active');
+  }
+  
+  // Activate the "Monthly Plan" tab and its content by default
+  window.addEventListener('DOMContentLoaded', function () {
+    openTab('monthly');
+  });
+  
